c9d0b261bc64b74531db26bd08c28513d0bd1ed7
Robert Love <robert@607D_Work.localdomain>
2022-10-04 10:03:08 -0500

    first codealong: method::main and variable assignments

---
+++ codeAlong_01/Program.cs
@@ -0,0 +1,42 @@
+﻿Using System;
+ 
+
+namespace CodeAlong_01{
+
+    class Program {
+
+        static void Main(string[] args){
+        // I want to print to the console my name and age 
+        // and a brief description of myself;
+        v// What do we know about C-Sharp
+    // We still have VALUE-TYPES
+    // strings, integers, arrays, floats, booleans
+
+
+                        
+                //type    name        value
+                int biggestNumberOnEarth = 0;
+
+                // let biggestNumberOnEarth =0;
+                bool isComplete = false;
+
+                // how do I make a string
+
+                string myGuitar = "ghost";
+
+                int[] ages = {21,22,23,24,25,26,27,28,29,30,31};
+                // _____________________________________________________________
+                
+            string myName = "Marcus";
+
+            int marcusAge = 25;
+
+            string marcusJob = "softwareDeveloper";
+
+            string funFact = "I absolutely loves swimming!";
+
+            Console.WriteLine($"my name is {myName}, I am {marcusAge} years old. For a living, I am a {marcusJob}. Fun Fact: {funFact}");
+        }
+    }
+}
+
